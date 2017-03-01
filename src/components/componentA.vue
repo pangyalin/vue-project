70aadf9a4f3d991dff04c4e2139394c6937321f6
pangyalin <pylpyl0714@126.com>
2017-03-01 22:42:49 +0800

    提交

---
+++ src/components/componentA.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+    <h2>{{msgfromfather}}</h2>
+    <button v-on:click="onClickme">open mouse!</button>
+  </div>
+</template>
+
+<script>
+
+  export default{
+    data :function(){
+      return {
+        msg:'hello from component a!'
+      }
+    },
+    watch:{
+
+    },
+    props:['msgfromfather'],
+//    子组件必须在props里注册 我接受这个属性
+    methods : {
+      onClickme:function () {
+        console.log(this.msgfromfather)
+        this.$emit('child-tell-me-something',this.msg);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yalin on 17/2/28.
+ */
+const STORAGE_KEY = 'todos-vuejs'
+export default{
+  fetch : function () {
+    return JSON.parse(window.localStorage.getItem(STORAGE_KEY)||'[]');
+  },
+  save : function (items) {
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
+  }
+}
+
